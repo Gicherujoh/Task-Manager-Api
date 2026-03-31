039c9d56b553ee55cf7da49ef90f31325bdf9c6e
Gicherujoh <johnwambugu236@gmail.com>
2026-04-01 01:00:03 +0300

    fix urls

--- server/app.js
+++ server/app.js
@@ -10,8 +10,21 @@ dotenv.config();
 const app = express();
 
 //const URL = process.env.FRONTEND_URL
+const allowedOrigins = [
+  process.env.FRONTEND_LOCAL_URL, // local frontend
+  process.env.FRONTEND_URL  // deployed frontend
+];
 
-app.use(cors({ origin:"https://task-manager-api-uwgu.vercel.app"}));
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); 
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 app.use(express.json());
 
 app.use("/api", taskRoutes);

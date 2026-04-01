446ee80e2ecdc8efca26430852112c520721fdfc
Gicherujoh <johnwambugu236@gmail.com>
2026-04-01 07:49:14 +0300

    fix urls

--- frontend/client/src/services/api.js
+++ frontend/client/src/services/api.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
 // frontend/api.js
-const baseURL = import.meta.env.MODE === "development"
-    ? `${import.meta.env.VITE_BACKEND_LOCAL}/api`
-    : import.meta.env.VITE_BACKEND_URL; // This already has /api
+// const baseURL = import.meta.env.MODE === "development"
+//     ? `${import.meta.env.VITE_BACKEND_LOCAL}/api`
+//     : import.meta.env.VITE_BACKEND_URL; // This already has /api
+const baseURL = import.meta.env.VITE_BACKEND_URL 
+  ? import.meta.env.VITE_BACKEND_URL 
+  : "http://localhost:5000/api";
 
-    console.log(import.meta.env.VITE_BACKEND_URL)
+console.log("Active API URL:", baseURL);
+  console.log("Axios Base URL:", baseURL); //
 const API = axios.create({ baseURL });
 
 export const getTasks = (status) =>
